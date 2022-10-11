f4c793db3ab68a6cf705e56e900be7db8d3a0286
URK21CS1092 <110010858+URK21CS1092@users.noreply.github.com>
2022-10-11 11:31:50 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,20 +1,19 @@
 from typing import List
-
-
-def selectionSort(array, size) -> List[int]: # Write your code here
+def selectionSort(array, size) -> List[int]:
+# Write your code here
 for i in range(size):
 min = i
 for j in range(i + 1, size):
 if array[min] > array[j]:
 min = j;
-array[i], array[min] = array[min], array[i] return array
-
-# Do not change the following code input_data = input()
+array[i], array[min] = array[min], array[i]
+return array
+# Do not change the following code
+input_data = input()
 data = []
-for item in input_data.split(', '): if item.isnumeric(): data.append(int(item))
-elif item.lstrip("-").isnumeric(): data.append(int(item))
- 
-
-
-
+for item in input_data.split(', '):
+if item.isnumeric():
+data.append(int(item))
+elif item.lstrip("-").isnumeric():
+data.append(int(item))
 print(selectionSort(data, len(data)))
